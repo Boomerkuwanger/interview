80f4e79e4d9f5b27a0499b3613bca2bc73ec9ffb
Anthony Fammartino <afammartino@godaddy.com>
2016-04-12 19:03:38 -0700

    initial test

---
+++ tests/unit/test_production_class.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from mock import patch
+
+
+'''
+Test the ProductionClass
+
+Three known valid values of the  "foo"  attribute
+in the  "ProductionClass"  are listed below:
+1.) 'addams_family'
+2.) u'yadda'
+3.) 'subzero'
+'''
+
+
+class TestProductionClass(TestCase):
+    # Write Tests here
+    pass
